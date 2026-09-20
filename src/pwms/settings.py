d158d5bdcb4d08de3a38327e719860ca4ef39945
Gavin Elie <gavin.elie@gmail.com>
2026-09-20 15:19:49 +0200

    Updated settings.py for DEV

--- src/pwms/settings.py
+++ src/pwms/settings.py
@@ -31,7 +31,13 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "workflows-dev.parliament.gov.za",
+    "pwmsdev",
+    "172.16.0.114",
+    "127.0.0.1",
+    "localhost",
+]
 
 # Application definition
 
